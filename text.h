7538dd26f65d2f11fa1ccd1e9685a1b2c8a0018a
janpaul7 <150805380+janpaul7@users.noreply.github.com>
2025-02-18 10:07:45 +0100

    Update text.h
    18.02.2025
    Funktionen aus main() gelöscht und der klasse Text hinzugefügt

--- text.h
+++ text.h
@@ -1,17 +1,25 @@
 #include <string>
 #include <vector>
+#include <chrono>
+#include <thread>
 
 class Text{
 private:
 unsigned int textID_m = 0;
 std::string textContent_m = "";
+static unsigned int msPrintDelay_m = 50;
 
 public:
 Text();
+
+void PrintDelay(unsigned int ms);
 
 void SetTextID(unsigned int textID);
 unsigned int GetTextID();
 
 void SetTextContent(std::string textContent);
 std::string GetTextContent();
+
+void SetmsPrintDelay(unsigned int msPrintDelay);
+unsigned int GetmsPrintDelay();
 };
