ef6e0264600284e4f23c3fa68de8efdaf5271019
janpaul7 <150805380+janpaul7@users.noreply.github.com>
2025-02-20 11:31:28 +0100

    Update main.cpp
    20.02.2025

--- main.cpp
+++ main.cpp
@@ -1,9 +1,30 @@
+#pragma once
 #include <iostream>
-#include "item.h"
 #include "text.h"
+#include "item.h"
 #include "zone.h"
+#include "spieler.h"
+
 
 int main()
 {    
+    //DEBUG INITS
+    Spieler currentPlayer;
+
+    Text DebugText1;
+    DebugText1.SetTextContent("Hello World!, Hello World!, Hello World!, Hello World!, Hello World!, Hello World!");
+
+    Zone DebugZone1;
+    DebugZone1.SetZoneText(DebugText1);
+
+    currentPlayer.SetCurrentZone(DebugZone1);
+
+    //GAME LOOP
+    while (currentPlayer.GetPlaying())
+    {
+        currentPlayer.PrintCurrentZoneText();
+        currentPlayer.AskForInput();
+        currentPlayer.CheckInput();
+    }
     return 0;
 }
