28ba53d2fe4ac76424a577759a9dd0677045c25d
janpaul7 <150805380+janpaul7@users.noreply.github.com>
2025-02-20 11:29:06 +0100

    Update spieler.h
    20.02.2025

--- spieler.h
+++ spieler.h
@@ -1,11 +1,16 @@
-#include "item.h"
+#pragma once
+#include "zone.h"
 #include <string>
 #include <vector>
 
 class Spieler{
 private:
 std::string userInput_m = "";
-std::vector<Item*> spielerInv;
+std::vector<Item> spielerInventory;
+Zone currentZone_m;
+bool playing = true;
+
+//Flags:
 
 public:
 Spieler();
@@ -13,4 +18,13 @@ Spieler();
 void SetUserInput(std::string userInput);
 std::string GetUserInput();
 
+void Spieler::SetCurrentZone(Zone currentZone);
+Zone GetCurrentZone();
+
+bool GetPlaying();
+
+void AddItemToInventory(Item addItem);
+void PrintCurrentZoneText();
+void AskForInput();
+void CheckInput();
 };
