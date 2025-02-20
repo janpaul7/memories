f063599d18e7b578a6fde0649f57a72cbc1b219b
janpaul7 <150805380+janpaul7@users.noreply.github.com>
2025-02-20 11:28:42 +0100

    Update spieler.cpp
    20.02.2025

--- spieler.cpp
+++ spieler.cpp
@@ -1,9 +1,12 @@
+#pragma once
+#include <iostream>
 #include "spieler.h"
 
 Spieler::Spieler()
 {
 
 }
+
 
 void Spieler::SetUserInput(std::string userInput)
 {
@@ -13,4 +16,46 @@ void Spieler::SetUserInput(std::string userInput)
 std::string Spieler::GetUserInput()
 {
     return this->userInput_m;
+}
+
+
+void Spieler::SetCurrentZone(Zone currentZone)
+{
+    this->currentZone_m = currentZone;
+}
+
+Zone Spieler::GetCurrentZone()
+{
+    return this->currentZone_m;
+}
+
+
+bool Spieler::GetPlaying()
+{
+    return this->playing;
+}
+
+
+void Spieler::AddItemToInventory(Item addItem)
+{
+    this->spielerInventory.push_back(addItem);
+}
+
+void Spieler::PrintCurrentZoneText()
+{
+    this->currentZone_m.GetZoneText().PrintDelay();
+}
+
+void Spieler::AskForInput()
+{
+    std::string input;
+    std::cout << std::endl << ">    ";
+    std::cin >> input;
+    this->userInput_m = input;
+    std::cout << std::endl;
+}
+
+void Spieler::CheckInput()
+{
+
 }
