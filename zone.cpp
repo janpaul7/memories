f29f630cfa4c45ae22234bd6f43fda110c07f331
janpaul7 <150805380+janpaul7@users.noreply.github.com>
2025-02-20 11:30:36 +0100

    Update zone.cpp
    20.02.2025

--- zone.cpp
+++ zone.cpp
@@ -1,3 +1,4 @@
+#pragma once
 #include "zone.h"
 
 std::vector<Zone*> zoneContainer;
@@ -7,6 +8,7 @@ Zone::Zone()
     zoneContainer.push_back(this);
     SetZoneID(zoneContainer.size());
 }
+
 
 void Zone::SetZoneID(unsigned int zoneID)
 {
@@ -18,6 +20,7 @@ unsigned int Zone::GetZoneID()
     return this->zoneID_m;
 }
 
+
 void Zone::SetZoneName(std::string zoneName)
 {
     this->zoneName_m = "\033[1;31m" + zoneName + "\033[1;0m";
@@ -26,4 +29,32 @@ void Zone::SetZoneName(std::string zoneName)
 std::string Zone::GetZoneName()
 {
     return this->zoneName_m;
+}
+
+
+void Zone::SetZoneText(Text zoneText)
+{
+    this->zoneText_m = zoneText;
+}
+
+Text Zone::GetZoneText()
+{
+    return this->zoneText_m;
+}
+
+
+void Zone::SetZoneItem(Item zoneItem)
+{
+    this->zoneItem_m.push_back(zoneItem);
+}
+
+Item Zone::GetZoneItem(std::string itemName)
+{
+    for (Item i : this->zoneItem_m)
+    {
+        if (i.GetItemName() == itemName)
+        {
+            return i;
+        }
+    }
 }
