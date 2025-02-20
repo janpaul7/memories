2efd1ff1b6b8e163b7559f58f4cfb792daf21c5b
janpaul7 <150805380+janpaul7@users.noreply.github.com>
2025-02-20 11:31:03 +0100

    Update zone.h
    20.02.2025

--- zone.h
+++ zone.h
@@ -1,10 +1,15 @@
+#pragma once
 #include <string>
 #include <vector>
+#include "item.h"
+#include "text.h"
 
 class Zone{
 private:
 unsigned int zoneID_m = 0;
 std::string zoneName_m = "";
+std::vector<Item> zoneItem_m;
+Text zoneText_m;
 
 public:
 Zone();
@@ -14,4 +19,10 @@ unsigned int GetZoneID();
 
 void SetZoneName(std::string zoneName);
 std::string GetZoneName();
+
+void SetZoneText(Text zoneText);
+Text GetZoneText();
+
+void SetZoneItem(Item zoneItem);
+Item GetZoneItem(std::string itemName);
 };
