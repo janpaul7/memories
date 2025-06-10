fcf715a0e2d785346e631942b96dcb5ca056e80a
janpaul7 <150805380+janpaul7@users.noreply.github.com>
2025-06-10 20:04:59 +0200

    Update spieler.h

--- spieler.h
+++ spieler.h
@@ -6,12 +6,22 @@
 class Spieler{
 private:
 std::string userInput_m = "";
-std::vector<Item*> spielerInventory;
 Zone currentZone_m;
 bool playing_m = true;
 bool isDebug_m = false;
 
 //Flags:
+bool hatCD = false;
+bool hatWerkzeugsatz = false;
+bool hatcode = false;
+bool torOffen = false;
+bool hatFlurSchonBetrten = false;
+bool schaltschrankoffen = false;
+bool schaltschrankoffenfirst = false;
+bool hatSD = false;
+
+bool terminalgeloest = false;
+bool konsolegeloest = false;
 
 public:
 Spieler();
@@ -19,24 +29,54 @@ Spieler();
 void SetUserInput(std::string userInput);
 std::string GetUserInput();
 
-void Spieler::SetCurrentZone(Zone currentZone);
+void SetCurrentZone(Zone currentZone);
 Zone GetCurrentZone();
 
 bool GetPlaying();
+void SetPlaying(bool playing);
 
 bool GetDebug();
 
-void AddItemToInventory(Item* addItem);
 void PrintCurrentZoneText();
 void AskForInput();
 bool CheckCMD(std::string input, std::string cmd);
-int CheckInputPosition(std::string input, std::string contains);
-void CheckInput(std::string input);
+short CheckInput(std::string input);
 
-void PrintInventar();
+std::string GetParameter(std::string befehl);
 void PrintUmschauen();
 void PrintHelpText();
+void PrintMemory();
 void PrintShutdown();
 void ToggleDebug();
 void PrintInvalid();
+
+void AufhebenCD();
+bool GetAufhebenCD();
+
+void SetHatCode();
+bool GetHatCode();
+
+void SetTorOffen();
+bool GetTorOffen();
+
+void SetHatWerkzeugsatz();
+bool GetHatWerkzeugsatz();
+
+void SetHatFlurSchonBetreten();
+bool GetHatFlurSchonBetreten();
+
+void Setschaltschrankoffen();
+bool Getschaltschrankoffen();
+
+void Setschaltschrankoffenfirst();
+bool Getschaltschrankoffenfirst();
+
+void SetHatSD();
+bool GetHatSD();
+
+void SetKonsoleGeloest();
+bool GetKonsoleGeloest();
+
+void SetTerminalGeloest();
+bool GetTerminalGeloest();
 };
