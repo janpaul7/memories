47df0fcd08f3f2a091c5e87e3aebe23a58f261db
janpaul7 <150805380+janpaul7@users.noreply.github.com>
2025-06-10 20:05:41 +0200

    Update zone.h

--- zone.h
+++ zone.h
@@ -1,15 +1,22 @@
 #pragma once
 #include <string>
 #include <vector>
-#include "item.h"
 #include "text.h"
 
 class Zone{
 private:
 unsigned int zoneID_m = 0;
 std::string zoneName_m = "";
-std::vector<Item> zoneItem_m;
 Text zoneText_m;
+Text zoneTextMemory_m;
+Text zoneTextUmschauen_m;
+
+Text zoneTextUmschauenItem1_m;
+Text zoneTextUmschauenItem2_m;
+
+//FLAGS
+bool Item1Aufgehoben = false;
+bool Item2Aufgehoben = false;
 
 public:
 Zone();
@@ -23,6 +30,21 @@ std::string GetZoneName();
 void SetZoneText(Text zoneText);
 Text GetZoneText();
 
-void SetZoneItem(Item zoneItem);
-Item GetZoneItem(std::string itemName);
+void SetZoneTextMemory(Text zoneTextMemory);
+Text GetZoneTextMemory();
+
+void SetZoneTextUmschauen(Text zoneTextUmschauen);
+Text GetZoneTextUmschauen();
+
+void SetzoneTextUmschauenItem1(Text zoneTextUmschauenItem1);
+Text GetzoneTextUmschauenItem1();
+
+void SetItem1Aufgehoben(bool aufgehoben);
+bool GetItem1Aufgehoben();
+
+void SetItem2Aufgehoben();
+bool GetItem2Aufgehoben();
+
+void SetzoneTextUmschauenItem2(Text zoneTextUmschauenItem2);
+Text GetzoneTextUmschauenItem2();
 };
